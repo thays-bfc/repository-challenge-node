e71a646b6efd58bd3b01ef67b4bfb04189302aa1
Thays Barbara <thaysbarbara@hotmail.com>
2020-04-30 22:03:21 -0300

    Challenge implementation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,59 @@ app.use(cors());
 
 const repositories = [];
 
+function checkRepositoryExists (request, response, next) {
+  const { id } = request.params;
+  var repository = repositories.find(r => r.id === id);
+  if (!repository) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+  request.repository=repository;
+
+   return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository =
+  {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
+  }
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", checkRepositoryExists,(request, response) => {
+
+  const { title, url, techs } = request.body;
+
+  request.repository.title = title;
+  request.repository.url = url;
+  request.repository.techs = techs;
+
+  return response.status(200).json(request.repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", checkRepositoryExists, (request, response) => {
+  
+  const index = repositories.findIndex(r => r.id === request.repository.id);
+
+  repositories.splice(index,1);
+
+  return response.status(204).json(repositories);
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", checkRepositoryExists, (request, response) => {
+  request.repository.likes +=1;
+  return response.status(200).json(request.repository);
 });
 
 module.exports = app;
